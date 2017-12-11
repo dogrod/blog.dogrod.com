c1c072e9b8b1563b61b218e970eb8577a560e2a4
dogrod <rodrickjue@hotmail.com>
2017-12-11 14:11:47 +0800

    :white_check_mark: Add test for index page

--- blogpost/tests.py
+++ blogpost/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
+from blogpost.views import index, view_post
 
 # Create your tests here.
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+      found = resolve('/')
+      self.assertEqual(found.func, index)
+        
