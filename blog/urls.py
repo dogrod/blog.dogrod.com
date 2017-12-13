738a5bc98250022a2800618f0321e9db65793024
dogrod <rodrickjue@hotmail.com>
2017-12-13 14:11:29 +0800

    :fire: Remove upload module
    Change upload saving location from local to UpYun

--- blog/urls.py
+++ blog/urls.py
@@ -22,4 +22,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('post.urls', namespace='post', app_name='post')),
     url(r'^admin/', include(admin.site.urls))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
