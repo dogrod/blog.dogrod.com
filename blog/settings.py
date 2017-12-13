584294ed252ea68a58233bd4c68db0a19d42d5bb
dogrod <rodrickjue@hotmail.com>
2017-12-13 11:16:11 +0800

    :sparkles: Add media upload feature (save to local)

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'post',
+    'media',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'dist/static/')
+
+MEDIA_URL = '/upload/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload').replace('\\', '/')
