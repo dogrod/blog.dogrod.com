aeb28774c37b7049e2c59757ad0994c6d71b38ba
dogrod <rodrickjue@hotmail.com>
2017-12-13 16:51:51 +0800

    :fire: Remove useless apps

--- media/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-from django.db import models
-
-# Create your models here.
-class Media(models.Model):
-  description = models.CharField(max_length=100)
-  image = models.ImageField(upload_to='image')
-  created = models.DateTimeField(auto_now_add = True)
