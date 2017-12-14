7cc6bff58d7d6bba936c0569f64107264f31e9ad
dogrod <rodrickjue@hotmail.com>
2017-12-14 16:00:56 +0800

    :sparkles: Add comment feature for post detail, follow from Django by Example Ch. 2

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_detail(request, year, month, day, slug):
   post = get_object_or_404(Post, slug=slug, status='published', publish_at__year=year, publish_at__month=month, publish_at__day=day)
+  
+  # List of active comments for this post
+  comments = post.comments.filter(active = True)
+
+  if request.method == 'POST':
+    # A comment was posted
+    comment_form = CommentForm(data = request.POST)
+
+    if comment_form.is_valid():
+      # Create Comment object but don't save to database yet
+      new_comment = comment_form.save(commit = False)
+      # Assign the current post to the comment
+      new_comment.post = post
+      # Save the comment to the database
+      new_comment.save()
+  
+  else:
+    comment_form = CommentForm()
+
   return render(request, 'post/detail.html', {
     'post': post,
+    'comments': comments,
+    'comment_form': comment_form
   })
 
 class PostListView(ListView):
