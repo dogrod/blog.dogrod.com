52a71e50c4f6e367b5d8792ef3e2596357089e16
dogrod <rodrickjue@hotmail.com>
2017-12-14 14:52:29 +0800

    :hammer: Delete render function of post list

--- blog/views.py
+++ blog/views.py
@@ -5,27 +5,6 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
-def post_list(request):
-  object_list = Post.published.all()
-  paginator = Paginator(object_list, 3) # 3 posts in each page
-
-  page = request.GET.get('page')
-
-  try:
-    posts = paginator.page(page)
-  except PageNotAnInteger:
-    # If page is not an integer deliver the first page
-    # such as undefined or character
-    posts = paginator.page(1)
-  except EmptyPage:
-    # If page is out of range deliver last page of results
-    posts = paginator.page(paginator.num_pages)
-
-  return render(request, 'post/list.html', {
-    'page': page,
-    'posts': posts
-  })
-
 def post_detail(request, year, month, day, slug):
   post = get_object_or_404(Post, slug=slug, status='published', publish_at__year=year, publish_at__month=month, publish_at__day=day)
   return render(request, 'post/detail.html', {
