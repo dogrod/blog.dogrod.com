733f875622ff4ef5857ee8eb3033eb9f2bbcf7ab
dogrod <rodrickjue@hotmail.com>
2017-12-15 13:31:37 +0800

    :hammer: Complete tag feature

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import CommentForm
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
+def post_list(request, tag_slug = None):
+  object_list = Post.published.all()
+  tag = None
+
+  if tag_slug:
+    tag = get_object_or_404(Tag, slug = tag_slug)
+    object_list = object_list.filter(tags__in = [tag])
+
+  paginator = Paginator(object_list, 3) # 3 posts in each page
+
+  page = request.GET.get('page')
+
+  try:
+    posts = paginator.page(page)
+  except PageNotAnInteger:
+    # If page is not an integer deliver the first page
+    # such as undefined or character
+    posts = paginator.page(1)
+  except EmptyPage:
+    # If page is out of range deliver last page of results
+    posts = paginator.page(paginator.num_pages)
+
+  print(posts)
+
+  return render(request, 'post/list.html', {
+    'page': page,
+    'posts': posts,
+    'tag': tag
+  })
+
+
 def post_detail(request, year, month, day, slug):
-  post = get_object_or_404(Post, slug=slug, status='published', publish_at__year=year, publish_at__month=month, publish_at__day=day)
+  post = get_object_or_404(Post, slug = slug, status = 'published', publish_at__year = year, publish_at__month = month, publish_at__day = day)
   
   # List of active comments for this post
   comments = post.comments.filter(active = True)
+  new_comment = None
 
   if request.method == 'POST':
     # A comment was posted
@@ -23,13 +56,6 @@ def post_detail(request, year, month, day, slug):
       new_comment.post = post
       # Save the comment to the database
       new_comment.save()
-      
-      return render(request, 'post/detail.html', {
-        'post': post,
-        'comments': comments,
-        'comment_form': comment_form,
-        'new_comment': new_comment
-      })
   
   else:
     comment_form = CommentForm()
@@ -37,7 +63,8 @@ def post_detail(request, year, month, day, slug):
   return render(request, 'post/detail.html', {
     'post': post,
     'comments': comments,
-    'comment_form': comment_form
+    'comment_form': comment_form,
+    'new_comment': new_comment
   })
 
 class PostListView(ListView):
