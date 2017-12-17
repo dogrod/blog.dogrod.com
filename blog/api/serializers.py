bcc09a2dd517ffe750bd504ac580681a2d096392
dogrod <rodrickjue@hotmail.com>
2017-12-17 16:55:42 +0800

    :sparkles: Add tags in post API

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -7,15 +7,24 @@ class CommentSerializer(serializers.ModelSerializer):
     model = Comment
     fields = ('id', 'name', 'body', 'create_at')
 
+class StringListField(serializers.ListField): # get from http://www.django-rest-framework.org/api-guide/fields/#listfield
+  child = serializers.CharField()
+
+  def to_representation(self, data):
+    return ' '.join(data.values_list('name', flat=True)) # you change the representation style here.
+
 class PostSerializer(serializers.ModelSerializer):
+  tags = StringListField()
+
   class Meta:
     model = Post
-    fields = ('id', 'title', 'slug', 'author', 'publish_at') # TODO: 'tags' is a TagglableManager
+    fields = ('id', 'title', 'slug', 'author', 'publish_at', 'tags')
 
 class PostDetailSerializer(serializers.ModelSerializer):
+  tags = StringListField()
   comments = CommentSerializer(many = True)
 
   class Meta:
     model = Post
-    fields = ('id', 'title', 'slug', 'author', 'publish_at', 'comments') # TODO: 'tags' is a TagglableManager
+    fields = ('id', 'title', 'slug', 'author', 'publish_at', 'comments', 'tags')
 
