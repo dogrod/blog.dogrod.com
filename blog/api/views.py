1d1dd5d4b0d601b667aec8b0f5b665dfd487d1ea
dogrod <rodrickjue@hotmail.com>
2017-12-21 20:03:51 +0800

    :hammer: Modify custome API response

--- blog/api/views.py
+++ blog/api/views.py
@@ -9,8 +9,21 @@ class PostListView(generics.ListAPIView):
   queryset = Post.published.all()
   serializer_class = PostSerializer
 
+  def list(self, request):
+    queryset = self.get_queryset()
+    serializer = PostSerializer(queryset, many = True)
+
+    return Response({
+      'posts': serializer.data
+    })
+
 class PostDetailView(generics.RetrieveAPIView):
   queryset = Post.published.all()
   serializer_class = PostDetailSerializer
 
+  def retrieve(self, request, pk = None):
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, pk = pk)
+    serializer = PostDetailSerializer(post)
+    return Response(serializer.data)
 # class CommentView(APIView):
