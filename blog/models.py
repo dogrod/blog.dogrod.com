3306d6c9ca40de64b512588791e4ef1d224bf27f
dogrod <rodrickjue@hotmail.com>
2017-12-26 18:18:10 +0800

    :hammer: Add author property to Comment in Post

--- blog/models.py
+++ blog/models.py
@@ -72,6 +72,7 @@ class Comment(models.Model):
   post = models.ForeignKey(Post, related_name = 'comments')
   name = models.CharField(max_length = 80)
   email = models.EmailField()
+  author = models.ForeignKey(User, related_name='post_comments', blank=True, null=True)
   content = models.TextField(verbose_name = u'content')
   create_at = models.DateTimeField(auto_now_add = True)
   update_at = models.DateTimeField(auto_now = True)
