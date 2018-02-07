41844da184d48df89ad395b558ef99649cc4dbd7
dogrod <rodrickjue@hotmail.com>
2018-02-07 13:20:58 +0800

    :hammer: Urls change

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     # post list
-    url(r'^posts', views.PostListView.as_view(), name='post_list'),
+    url(r'^posts$', views.PostListView.as_view(), name='post_list'),
     # post detail
-    url(r'^post/(?P<slug>[-\w]+)',
+    url(r'^posts/(?P<slug>[-\w]+)',
         views.PostDetailView.as_view(),
         name='post_detail'),
     url(r'^tags', views.TagsView.as_view(), name='tag_list'),
-    url(r'^tag/(?P<tag_slug>[-\w]+)',
+    url(r'^tags/(?P<tag_slug>[-\w]+)',
         views.PostListView.as_view(),
         name='post_list_as_tag')
 ]
