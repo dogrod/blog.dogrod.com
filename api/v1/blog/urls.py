a9d90a47a9e63e3cc2a74f61441a0ba79b141070
dogrod <rodrickjue@hotmail.com>
2018-07-03 18:23:53 +0800

    :fire: Change urls rules

--- api/v1/blog/urls.py
+++ api/v1/blog/urls.py
@@ -5,13 +5,13 @@ urlpatterns = [
     # Post list
     url(r'^posts$', views.PostListView().as_view(), name='post_list'),
     # Post detail
-    url(r'^post/(?P<post_slug>[-\w]+)',
+    url(r'^posts/(?P<post_slug>[-\w]+)',
         views.PostDetailView.as_view(),
         name='post_detail'),
-    # Tag list API
-    url(r'^tags$', views.TagsView.as_view(), name='tag_list'),
-    # Tag detail API, return a post list with tag
-    url(r'^tag/(?P<tag_name>[-\w]+)',
+    # Tagged post list API, return a post list with tag
+    url(r'^/posts/tag/(?P<tag_name>[-\w]+)',
         views.PostListView().as_view(),
-        name='post_list_with_tag')
+        name='post_list_with_tag'),
+    # Tag list API
+    url(r'^tags$', views.TagsView.as_view(), name='tag_list')
 ]
