ff9f6bba18db53133b62987f921fff11cc7f948a
dogrod <rodrickjue@hotmail.com>
2018-08-31 17:17:53 +0800

    :wrench: Remove Session Based Auth

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,7 +157,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'mysite.pagination.CommonListPagination',
