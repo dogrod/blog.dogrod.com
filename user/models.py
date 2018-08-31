87945ac5fadf505b3d39867495d881d42c48488d
dogrod <rodrickjue@hotmail.com>
2018-08-31 16:14:52 +0800

    :hammer: All fields of UserProfile is nullable

--- user/models.py
+++ user/models.py
@@ -7,19 +7,19 @@ from django.dispatch import receiver
 # Profile to User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.CharField(max_length=120, blank=True)
-    nick_name = models.CharField(max_length=20, blank=True)
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=50, blank=True)
-    age = models.IntegerField(blank=True)
+    avatar = models.CharField(max_length=120, blank=True, null=True)
+    nick_name = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(self, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
-def save_user_profile(self, instance, **kwargs):
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
