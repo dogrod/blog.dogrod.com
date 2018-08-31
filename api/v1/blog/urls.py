5f7660f0b64963cfc1a433fca8c44d65fd30fff9
dogrod <rodrickjue@hotmail.com>
2018-08-31 15:33:26 +0800

    :hammer: Url rules changed

--- api/v1/blog/urls.py
+++ api/v1/blog/urls.py
@@ -12,18 +12,18 @@ post_detail = PostViewSet.as_view({
 
 urlpatterns = [
     # Post list
-    url(r'^posts$', post_list, name='post_list'),
+    url(r'^/posts$', post_list, name='post_list'),
     # Post detail
-    url(r'^posts/(?P<post_id>[-\w]+)$',
+    url(r'^/posts/(?P<post_id>[-\w]+)$',
         post_detail,
         name='post_detail'),
     # Like post
-    url(r'^posts/(?P<post_id>[-\w]+)/like$',
+    url(r'^/posts/(?P<post_id>[-\w]+)/like$',
         views.LikePostAPIView.as_view(),
         name='like_post'),
-    url(r'^posts/(?P<post_id>[-\w]+)/comments$',
+    url(r'^/posts/(?P<post_id>[-\w]+)/comments$',
         views.CommentAPIView.as_view(),
         name='comment_post'),
     # Tag list API
-    url(r'^tags$', views.TagsView.as_view(), name='tag_list')
+    url(r'^/tags$', views.TagsView.as_view(), name='tag_list')
 ]
